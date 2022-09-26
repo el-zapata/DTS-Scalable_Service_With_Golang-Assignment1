6c7fb0fc8f2edb519ad8ddd53b07f9f202b89dcb
Zapata <elzapata.libertad@gmail.com>
2022-09-26 12:21:16 +0700

    Penambahan komentar untuk menjelaskan fungsi file struct.go

--- src/struct.go
+++ src/struct.go
@@ -1,11 +1,23 @@
+/* 
+-- Pendefinisian struct ada pada file ini
+*/
+
 package src
 
+/* 
+-- Disini memakai 2 struct, yaitu Person dan Student
+*/
+
+
+// Struct Person mempunyai field Nama, Alamat, dan Pekerjaan
 type Person struct {
 	Nama string
 	Alamat string
 	Pekerjaan string
 }
 
+/* Struct Student mempunyai field yang ada di Person ditambah
+Absen dan Alasan */
 type Student struct {
 	person Person	
 	Absen int
