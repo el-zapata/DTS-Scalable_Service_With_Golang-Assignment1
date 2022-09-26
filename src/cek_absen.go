9cd23df7fa58d6649892bfeab6be6d6e1ad7f32c
Zapata <elzapata.libertad@gmail.com>
2022-09-26 12:50:04 +0700

    Menghapus metode for [] range interface (Baru sadar sebenarnya tidak perlu menggunakan itu) dan label outerLoop

--- src/cek_absen.go
+++ src/cek_absen.go
@@ -4,17 +4,16 @@ import (
 	"fmt"
 )
 
-// Fungsi Cek_absen mengambil input parameter pointer dan nilai return bertipe string
+/*
+-- Fungsi Cek_absen mengambil input parameter pointer dan nilai return bertipe string
+-- Fungsi ini akan mengembalikan string variabel data, jika no absen ditemukan, data akan
+bernilai data dari siswa absen tersebut. Jika tidak, akan bernilai "Data siswa tidak
+ditemukan!!!""
+*/ 
+
 func Cek_absen(input *int) string {
 	data := "Data siswa tidak ditemukan!!!"
-	outerLoop:
 	for _, data_student := range Students {
-		for range []interface{} {	data_student.person.Nama, 
-									data_student.person.Alamat,
-									data_student.person.Pekerjaan,
-									data_student.Absen, 
-									data_student.Alasan,
-									} {
 		if (*input == data_student.Absen) {
 			data = fmt.Sprintf("Nama: %s\nAlamat: %s\nPekerjaan: %s\nAlasan memilih kelas Golang: %s\n",
 								data_student.person.Nama,
@@ -23,26 +22,8 @@ func Cek_absen(input *int) string {
 								data_student.Alasan)
 
 			// Akan break dari seluruh for loop sehingga tidak perlu cek data selanjutnya
-			break outerLoop 
-		}
+			break
 		}
 	}
 	return data
 }
-
-/* Alasan saya memakai empty interface adalah sebagai berikut:
-
-Tadinya saya mau iterate seluruh field di dalam satu data dengan:
-
-	for _, data_student := range Students {
-		for field, value := range data_student {
-			
-		}
-	}
-	
-Namun error: Cannot range over data_student (variable of type Student)
-
-Lalu saya melihat di stackoverflow bahwa untuk iterate value dari
-field bisa digunakan empty interface seperti di atas
-*/
-
